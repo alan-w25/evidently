9d6ddbefc5a6bc713e89d26d8db84116c0b8a4d6
Alan Wu <43737208+alan-w25@users.noreply.github.com>
2026-04-19 23:47:05 -0400

    Delete tests directory

--- tests/future/metrics/test_in_range_metric.py
+++
@@ -1,7 +0,0 @@
-from evidently.metrics import InRangeValueCount
-
-
-def test_in_range_metric():
-    metric = InRangeValueCount(column="a", left=0.5, right=1)
-    assert metric.left == 0.5
-    assert metric.right == 1
--- tests/test_torch_numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import torch
-
-
-def test_torch_to_numpy():
-    array = torch.tensor([1, 2, 3]).numpy()
-    assert np.array_equal(array, np.array([1, 2, 3]))
